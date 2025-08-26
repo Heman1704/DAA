e9b9a0f194589c51c4fddc1429f915417020fecd
hemant <hemantnayainjha7@gmail.com>
2025-08-26 17:23:48 +0530

    uploded struct

---
+++ exp4/Freq_ele.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<ArrayList<Integer>> countFreq(int[] arr) {
+        LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            ArrayList<Integer> pair = new ArrayList<>();
+            pair.add(entry.getKey());
+            pair.add(entry.getValue());
+            result.add(pair);
+        }
+        return result;
+    }
+}
+
